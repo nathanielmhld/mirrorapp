90e1537211e9b7076b504e5dd1b6106446978bcb
nathanielmhld <nathanielmhld@gmail.com>
2018-08-02 13:14:31 -0400

    Emptied media js

--- mirror/components/media.js
+++ mirror/components/media.js
@@ -6,37 +6,25 @@ import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityI
 import { RNS3 } from 'react-native-aws3';
 
 class MediaComponent extends Component{
-
-	state = {
-		configPhoto: null
+	constructor(props) {
+    super(props);
+    this.state = {
+      userID: -1
+    }
+    var intervalID = window.setInterval(checkServer, 1000);
 	}
-	async componentWillMount(){
-		const value = await AsyncStorage.getItem('configPhoto');
-    	if (value !== null) {
-    	this.setState({configPhoto: value})
-      	console.log("Mounting with: " + value);
-    	}
-	}
+    async checkServer(){
 
-	
-
+    }
 
 	render(){
-		const {configPhoto} = this.state
-		if(configPhoto === null)
-		{
-			return <View />
-		}else{
-		
 			return(
 
 			<View style={{flex:1}}>
 				<Text> HELLO </Text>
-				<Image source={{uri: configPhoto}} />
 			</View>
 
 			)
-		}
 		}
 	}
 
